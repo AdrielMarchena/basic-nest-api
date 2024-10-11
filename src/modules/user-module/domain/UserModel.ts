aecabd29a4647b5db0145282cf69d87a8693694a
MatheusMarchena <Matheus.Marchena@hotmail.com>
2024-10-10 21:30:20 -0300

    swagger

--- src/modules/user-module/domain/UserModel.ts
+++ src/modules/user-module/domain/UserModel.ts
@@ -1,8 +1,16 @@
-export interface UserModel {
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UserModel {
+    @ApiProperty({example: "123", default: "123"})
     id: string;
+    @ApiProperty()
     createdAt: Date;
+    @ApiProperty()
     updatedAt: Date;
+    @ApiProperty()
     name: string;
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     password: string;
 }
